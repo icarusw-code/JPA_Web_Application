701b30289266c80bff7988a573fbcc169b9e548e
icarusw-code <77170611+icarusw-code@users.noreply.github.com>
2022-01-29 23:08:03 +0900

    api develop v1-v6

--- jbashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jbashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
  */
 @RestController
 @RequiredArgsConstructor
-public class OrderSimpleApi {
+public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
     private final OrderSimpleQueryRepository orderSimpleQueryRepository;
